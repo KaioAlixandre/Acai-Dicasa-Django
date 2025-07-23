c92f87637d24ac495278de43c01cfebc2fbd3002
Kaio Alixandre <Kaioalexandre2681@gmail.com>
2025-07-23 19:36:50 -0300

    adicionado os caminhos das paginas

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pedido(request):
+    return render(request, 'pedidos/pedido.html')
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from produtos import views
+from pedidos import views as pedidos_views
+from usuarios import views as usuarios_views
 
 app_name = 'produtos'
 
@@ -8,4 +10,8 @@ urlpatterns = [
     path('', views.lista_produtos, name='lista_produtos'),
     path('cadastrar/', views.cadastrar_produto, name='cadastrar'),
     path('accounts/', include('django.contrib.auth.urls')),  # Adicione esta linha
+    path('promocoes/', views.promocoes, name='promocoes'),
+    path('pedido/', pedidos_views.pedido, name='pedido'),  
+    path('perfil/', usuarios_views.peril, name='perfil'),  
+
 ]
